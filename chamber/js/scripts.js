83afb9b927216052d15aa0bef55e50c7ea3942c2
elizabethtellez <tel21004@byui.edu>
2022-02-11 20:50:05 -0800

    week 6 changes

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,7 +1,11 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+  document.querySelector(".navbar").classList.toggle("white");
+  document.querySelector(".navbar").classList.toggle("no_border");
 }
+/*function background_border() {
+  
+}*/
 const x = document.getElementById("hamburgerBtn");
-x.onclick = toggleMenu
+x.onclick = toggleMenu;
